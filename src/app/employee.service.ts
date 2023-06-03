e1b9e98260ecc6dd2a603c7fc8fd4e2a5a899fd4
rajesh9442 <rajeshmahendran1999@gmail.com>
2023-06-03 13:12:58 -0500

    Modified service and appModule

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
 @Injectable({
     providedIn: 'root'
 })
 export class EmployeeService {
-    private apiServerUrl = '';
+    private apiServerUrl = environment.apiBaseUrl;
     constructor(private http: HttpClient) {}
 
     public getEmployees(): Observable<Employee[]> {
