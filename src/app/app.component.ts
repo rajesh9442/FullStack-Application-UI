abb3ab7d224e703ec33e222c05052d15a1f57365
rajesh9442 <rajeshmahendran1999@gmail.com>
2023-06-03 07:23:31 -0500

    Modified app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employeemanagerapp';
+  employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(){
+    this.getEmployees();
+  }
+
+  public getEmployees():void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[])=>{
+        this.employees = response;
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    )
+  }
 }
